50d198944924ed005d51e71292202c73a3b0fbc5
YellowFive <yellowfive555@yahoo.com>
2024-06-11 16:24:22 +0300

    tests fails

--- CubeSolver/Tests/FiguresGenerator/WhenGeneratingFigures.cs
+++ CubeSolver/Tests/FiguresGenerator/WhenGeneratingFigures.cs
@@ -1,5 +1,6 @@
 ﻿#region Usings
 
+using Core;
 using FluentAssertions;
 
 #endregion
@@ -16,5 +17,38 @@ public class WhenGeneratingFigures : FiguresGeneratorTestBase
         var figures = generator.GenerateFiguresSet();
 
         figures.Count.Should().Be(13);
+    }
+
+    [Test]
+    public void NumberOfFiguresColorsIsCorrect()
+    {
+        var generator = new Core.FiguresGenerator();
+
+        var figures = generator.GenerateFiguresSet();
+
+        figures.Count(f => f.Color is Color.Blue).Should().Be(4);
+        figures.Count(f => f.Color is Color.White).Should().Be(5);
+        figures.Count(f => f.Color is Color.Yellow).Should().Be(4);
+    }
+
+    [Test]
+    public void NumberOfFiguresDimensionsIsCorrect()
+    {
+        var generator = new Core.FiguresGenerator();
+
+        var figures = generator.GenerateFiguresSet();
+
+        figures.Count(f => f.Dimension == 1).Should().Be(3);
+        figures.Count(f => f.Dimension == 2).Should().Be(10);
+    }
+
+    [Test]
+    public void TotalNumberOfCubesInSetIs64()
+    {
+        var generator = new Core.FiguresGenerator();
+
+        var figures = generator.GenerateFiguresSet();
+
+        Assert.Fail(); // todo realization needed
     }
 }
